bcc43f3c61987f474a7e6eff062c11370b5bf011
peruibeloko <cmendonca32@gmail.com>
2025-12-02 18:23:31 -0300

    D1P1 make it pretty

--- src/Day1/Part1.cs
+++ src/Day1/Part1.cs
@@ -6,12 +6,12 @@ class Part1
     {
         int position = 50;
         int password = 0;
-        
+
         foreach (var line in input.Split("\n"))
         {
             var direction = line[0] == 'R' ? 1 : -1;
             var distance = int.Parse(line.Substring(1));
-            
+
             position = Move(position, direction, distance);
 
             if (position == 0) password++;
@@ -22,12 +22,7 @@ class Part1
 
     static int Move(int position, int direction, int distance)
     {
-        if (direction > 0) return (position + distance) % 100;
-
-        var offset = (position - distance) % 100;
-
-        if (offset < 0) return 100 + offset;
-        
-        return offset;
+        var newPos = (position + distance * direction) % 100;
+        return newPos < 0 ? 100 + newPos : newPos;
     }
 }
